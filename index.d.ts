b865fbeb44cf6cbc35a064e9adff3848a12eca63
qeffects <qfluxstudios@gmail.com>
2021-09-15 21:31:19 +0300

    update

--- index.d.ts
+++ index.d.ts
@@ -2,6 +2,7 @@
 declare type BasicListMember<V extends string> = Record<V, number>;
 
 /** This module assumes lower priority number is later in the list */
+/** @noSelf **/
 export declare const binarySearch: {
     /** Finds the first occurence of val
      * @param list A sorted list
@@ -9,6 +10,7 @@ export declare const binarySearch: {
      * @param val The number to be found
      * @returns The position of the member that contains the number 
      */
+
     basic: <V extends string>(list: BasicListMember<V>[], index: V, val: number) => number | false;
     /** Finds where to insert this value
      * @param list A sorted list
@@ -18,4 +20,3 @@ export declare const binarySearch: {
      */
     findInsert: <V_1 extends string>(list: BasicListMember<V_1>[], index: V_1, val: number) => number;
 };
-export {};
